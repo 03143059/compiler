1cf080f7c32452ce85ef42f2bdc501fadb950899
Werner Garcia <03143059@galileo.edu>
2014-11-08 12:16:43 -0600

    Added missing files

--- src/test/test.s
+++ src/test/test.s
@@ -1,150 +1,71 @@
-##################################
-# Global Declarations
-# Variables: Int a, Int b, Int c[3], Int d[10], Boolean e, Boolean f[2]
-##################################
-	.data
-var_a:	.word	1	# int a
-var_b:	.word	1	# int b
-var_c:	.word	0:3	# int c[3]
-var_d:	.word	0:10	# int d[10]
-var_e:	.word	1	# int e
-var_f:	.word	0:2	# int f[2]
-##################################
-# End Global
-##################################
-
-	.globl	main
-	.text
-	j	main	# jump to main program
-##################################
-# Function: gcd
-# Params: INT a, INT b
-# Returns: INT
-##################################
-gcd:
-	.data
-	.text
-	addi	$sp, $sp, -12
-	sw	$ra, 0($sp)	# reserve stack
-	sw	$a0, 4($sp)
-	sw	$a1, 8($sp)
-# if stmt
-	beqz	$a1, gcd_lbl2	# lleva un lc (label count) por metodo, brinca al lc+2 (true block)
-	
-gcd_lbl1: # false block
-
-	lw	$t0, 8($sp)	# load param t0 = b
-	
-	lw	$t1, 4($sp)	# load param t1 = a
-	
-	div	$t1, $t0	# div a/b y recuperar residuo
-	mfhi	$t2		# t2 = a%b
-	
-	move	$a0, $t0	# param1 = t0
-	move	$a1, $t2	# param2 = t2
-	jal	gcd		# gcd(t0, t2)	
-	move	$t0, $v0	# guardar valor de retorno
-	
-	move	$v0, $t0	# return stmt
-	j	gcd_end	
-	
-	j	gcd_lbl3	# salir del if
-	
-gcd_lbl2: # true block
-	move	$t0, $a0	# return statement, copy value to return
-	
-	move	$v0, $t0	# return stmt
-	j	gcd_end
-	
-gcd_lbl3: # end if
-gcd_end:
-	lw	$ra, 0($sp)	# restore stack
-	addi	$sp, $sp, 12
-	jr	$ra	# return
-##################################
-# End Function gcd
-##################################
-
-##################################
-# Function: test
-# Params: 
-# Returns: BOOLEAN
-##################################
-test:
-	.data
-	.text
-	addi	$sp, $sp, -4
-	sw	$ra, 0($sp)	# reserve stack
-var_i:	.word	1	# int i
-	li	$t0, 0		# t = 0
-test_for1:
-	bgt	$t0, 2, test_next1
-	sw	$t0, var_i	# i = t
-	
-	mul	$t1, $t0, $t0	# t = i*i
-	
-	lw	$t2, var_i	# load i, se usa t2 porque t0 y t1 ya estan utilizados
-	
-	sw	$t1, var_c($t2)	# c[i] = t
-	
-	addi	$t0, $t0, 1		# i++
-	j	test_for1
-test_next1:
-	return	$a, $b, $c
-test_end:
-	lw	$ra, 0($sp)	# restore stack
-	addi	$sp, $sp, 4
-	jr	$ra	# return
-##################################
-# End Function test
-##################################
-
-##################################
-# Function: test2
-# Params: 
-# Returns: VOID
-##################################
-test2:
-	.data
-	.text
-	addi	$sp, $sp, -4
-	sw	$ra, 0($sp)	# reserve stack
-	return	$a, $b, $c
-test2_end:
-	lw	$ra, 0($sp)	# restore stack
-	addi	$sp, $sp, 4
-	jr	$ra	# return
-##################################
-# End Function test2
-##################################
-
-##################################
-# Function: main
-# Params: 
-# Returns: INT
-##################################
-main:
-	.data
-	.text
-	addi	$sp, $sp, -4
-	sw	$ra, 0($sp)	# reserve stack
-var_x:	.word	1	# int x
-var_y:	.word	1	# int y
-var_z:	.word	1	# int z
-	sw	$a, $b($c)
-	sw	$a, $b($c)
-	sw	$a, $b($c)
-	sw	$a, $b($c)
-	sw	$a, $b($c)
-	j	printf
-	j	print_int
-	return	$a, $b, $c
-main_end:
-	lw	$ra, 0($sp)	# restore stack
-	addi	$sp, $sp, 4
-	jr	$ra	# return
-##################################
-# End Function main
-##################################
-
-
+PROGRAM START
+BEGIN GLOBAL SECTION
+Global Int a
+Global Int b
+Global Int[3] c
+Global Int[10] d
+Global Boolean e
+Global Boolean[2] f
+END GLOBAL SECTION
+Method (name: gcd, returns: INT, params: INT a, INT b)
+Evaluate boolean expression
+load immediate literal 0
+load b
+Execute binary operation: ==
+If result is true
+load a
+Execute command: return
+If result is false
+load function parameters
+load b
+load b
+load a
+Execute binary operation: %
+jump to function: gcd
+Execute command: return
+Method (name: test, returns: BOOLEAN)
+declare variable i
+use variable i
+load immediate literal 0
+load immediate literal 2
+load i
+load i
+Execute binary operation: *
+load c
+load i
+store with op Equal
+load immediate literal 1
+Execute command: return
+Method (name: test2, returns: VOID)
+Execute command: return
+Method (name: main, returns: INT)
+declare variable x
+declare variable y
+declare variable z
+load immediate literal 100
+load a
+store with op Equal
+load immediate literal 20
+load b
+store with op Equal
+load a
+load x
+store with op Equal
+load b
+load y
+store with op Equal
+load function parameters
+load x
+load y
+jump to function: gcd
+load z
+store with op Equal
+load function parameters
+load string %c
+load a
+jump to function: printf
+load function parameters
+load z
+jump to function: print_int
+load immediate literal 0
+Execute command: return
---
+++ src/test/unary.s
@@ -0,0 +1,28 @@
+PROGRAM START
+BEGIN GLOBAL SECTION
+END GLOBAL SECTION
+Method (name: main, returns: VOID)
+declare variable a
+declare variable b
+load immediate literal 10
+Unary operation: -
+load a
+store with op Equal
+load immediate literal 0
+Unary operation: !
+load b
+store with op Equal
+use variable a
+load immediate literal 1
+load immediate literal 3
+Evaluate boolean expression
+load immediate literal 2
+load a
+Execute binary operation: ==
+If result is true
+Execute command: break
+If result is false
+load immediate literal 1
+load b
+store with op Equal
+Execute command: continue
